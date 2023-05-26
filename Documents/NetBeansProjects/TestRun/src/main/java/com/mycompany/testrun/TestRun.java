fca18e8618eba51d86ba108f6dcbfd04662becb0
gittybro12 <umeahamsylvester@gmail.com>
2023-05-26 01:18:54 -0700

    TEST

---
+++ Documents/NetBeansProjects/TestRun/src/main/java/com/mycompany/testrun/TestRun.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.testrun;
+
+/**
+ *
+ * @author dell
+ */
+public class TestRun {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
